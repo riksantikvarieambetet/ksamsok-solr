41447c72a1ae1c8146efdab77a8fb2d48e4840de
rilar <rilar>
2013-02-20 11:23:10 +0000

    Skapar shellscript för att sätta upp miljön på ksamsökslaven.

---
+++ rpm/prep_slave.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to link in the index on the local filesystem.
+# Exptected to execute only on the slave ksamsokserver
+echo "We are on the SLAVE!"
+
+echo "About to create /var/lucene-index"
+mkdir /var/lucene-index
+chown tomcat:raagroup /var/lucene-index
+chmod 755 /var/lucene-index
+echo "Done preparing SLAVE!"
