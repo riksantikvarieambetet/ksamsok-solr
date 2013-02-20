ca2335425e98451d6f9dce8ef502614cc5c0bcd1
rilar <rilar>
2013-02-20 11:57:28 +0000

    Tar bort onödigt anrop...

--- rpm/prep_slave.sh
+++ rpm/prep_slave.sh
@@ -3,8 +3,9 @@
 # Exptected to execute only on the slave ksamsokserver
 echo "We are on the SLAVE!"
 
-echo "About to create /var/lucene-index"
-mkdir /var/lucene-index
-chown tomcat:raagroup /var/lucene-index
-chmod 755 /var/lucene-index
+
+#echo "About to create /var/lucene-index"
+#mkdir /var/lucene-index
+#chown tomcat:raagroup /var/lucene-index
+#chmod 755 /var/lucene-index
 echo "Done preparing SLAVE!"
