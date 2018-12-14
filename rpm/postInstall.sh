acc07d6df1533f7e6f6ebda876bda762335ac62d
Stefan Malmesjö <stefan.malmesjo@altrusoft.se>
2018-12-14 14:02:58 +0100

    KSAM-30
    Byt till solr 7.5
    
    Bytt från ant till gradle

---
+++ rpm/postInstall.sh
@@ -0,0 +1,9 @@
+#solr har ett installationsskript, som vi kör istället för att göra en "egen" installation av solr
+/tmp/solr-7.5.0/install_solr_service.sh /tmp/solr-7.5.0/solr-7.5.0.tgz -i /usr/local
+
+# installera ksamsok som en core
+su solr -c "/usr/local/solr/bin/solr create_core -c ksamsok -d /tmp/ksamsok-config"
+
+
+
+
