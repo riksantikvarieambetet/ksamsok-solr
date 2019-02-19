18ec96ea1d8d7aaeb4a2f8822e3aff9e9db0c14e
Stefan Malmesjö <stefan.malmesjo@altrusoft.se>
2019-02-19 21:30:48 +0100

    KSAM-214
    Ny sökväg till solrs datakatalog
    
    Rensa bättre efter avinstallation

--- rpm/postUninstall.sh
+++ rpm/postUninstall.sh
@@ -4,3 +4,10 @@ rm -f /etc/init.d/solr
 rm -rf /usr/local/solr
 rm -rf /usr/local/solr-7.5.0
 
+rm -f /etc/default/solr.in.sh
+rm -f /mnt/lucene-index/data/
+rm -f /mnt/lucene-index/data/solr.xml
+rm -f /mnt/lucene-index/log4j2.xml
+rm -rf /mnt/lucene-index/data/ksamsok/
+
+
