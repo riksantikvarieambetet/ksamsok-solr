0674dac9b9ccee746049771417a444394e1b9fc4
Stefan Malmesjö <stefan.malmesjo@altrusoft.se>
2019-02-19 17:16:29 +0100

    KSAM-214
    Ny sökväg till solrs datakatalog

--- rpm/postInstall.sh
+++ rpm/postInstall.sh
@@ -1,16 +1,15 @@
 #solr har ett installationsskript, som vi kör istället för att göra en "egen" installation av solr
-/tmp/solr-7.5.0/install_solr_service.sh /tmp/solr-7.5.0/solr-7.5.0.tgz -i /usr/local
+/tmp/solr-7.5.0/install_solr_service.sh /tmp/solr-7.5.0/solr-7.5.0.tgz -i /usr/local -d /mnt/lucene-index
 
 
-if [ ! -d /var/solr/data/ksamsok ]; then
+if [ ! -d /mnt/lucene-index/data/ksamsok ]; then
     # installera ksamsok som en core
     su solr -c "/usr/local/solr/bin/solr create_core -c ksamsok -d /tmp/ksamsok-config"
 else
     #installera konfigfilerna i ksamsok-conf
-    su solr -c "cp -r /tmp/ksamsok-config/* /var/solr/data/ksamsok/conf/"
+    su solr -c "cp -r /tmp/ksamsok-config/* /mnt/lucene-index/data/ksamsok/conf/"
 fi
 
-###### HÄR GÅR INTE ATT LÄGGA TILL NÅGOT MER, EFTERSOM SKRIPTET SKJUTER UT PÅ OVANSTÅENDE RAD OM KSAMSOK-COREN REDAN FINNS ######
 
 
 
